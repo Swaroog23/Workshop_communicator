75720cf8b44047d7237c85074cd81c3671b3db6c
Dominik Popławski <poplawd@gmail.com>
2020-08-04 19:47:56 +0200

    create db ver.2

---
+++ create_db.py
@@ -0,0 +1,17 @@
+import psycopg2
+
+
+def create_db(database_name):
+    try:
+        sql_db_create = 'CREATE DATABASE %s;', (database_name)
+        return sql_db_create
+    except DuplicateDatabase:
+        return "Database exist!"
+
+def create_user_table():
+    try:
+        sql_table = 'CREATE TABLE %s(%s serial primary key, \
+            %s varchar(255), %s varchar(80));', ('Users', 'id', 'username', 'hashed_password')
+        return sql_table
+    except DuplicateDatabase:
+        return "Database exists!"
