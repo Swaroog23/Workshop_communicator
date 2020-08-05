b61a1f2109fa1d54fa512277da9daab7ec2a2fd0
Dominik Popławski <poplawd@gmail.com>
2020-08-05 17:13:23 +0200

    Create_db + models.py-ver1

--- create_db.py
+++ create_db.py
@@ -1,32 +1,45 @@
 import psycopg2
 
+USER = 'postgres'
+PASSWORD = '@DoMInio1@'
+HOST = 'localhost'
+
 
 class DatabaseCreation:
 
     @staticmethod
     def create_db(database_name):
-        try:
-            sql_db_create = 'CREATE DATABASE %s;', (database_name)
-            return sql_db_create
-        except psycopg2.Error.DuplicateDatabase:
-            return "Database exist!"
+        sql_db_create = 'CREATE DATABASE %s;', (database_name)
+        return sql_db_create
 
     @staticmethod
     def create_user_table():
-        try:
-            sql_table = 'CREATE TABLE %s(%s serial primary key, \
-                %s varchar(255), %s varchar(80));', ('Users', 'id', 'username', 'hashed_password')
-            return sql_table
-        except psycopg2.Error.DuplicateDatabase:
-            return "Database exists!"
-            
+        sql_table = ("""CREATE TABLE %s(%s serial primary key, \n
+            %s varchar(255), %s varchar(80));""") % ('Users', 'id', 'username', 'hashed_password')
+        return sql_table
+
     @staticmethod
     def create_msg_table():
-        try:
-            sql_table = 'CREATE TABLE %s(%s serial primary key, \
-                %s int not null, %s int not null, %s timestamp not null) \
-                FOREIGN KEY(%s) REFERENCES Users(id) \
-                FOREGIN KEY(%s) REFERENCES Users(id);', ('Messeges', 'id', 'from_id', 'to_id', 'creation_date', 'from_id', 'to_id')
-            return sql_table
-        except psycopg2.Error.DuplicateDatabase:
-            return "Database exists!"
+        sql_table = '''CREATE TABLE %s(%s serial primary key, \n
+            %s int not null, %s int not null, %s timestamp not null, \n
+            FOREIGN KEY(%s) REFERENCES Users(id), \n
+            FOREIGN KEY(%s) REFERENCES Users(id)); ''' % ('Messeges', 'id', 'from_id', 'to_id', 'creation_date', 'from_id', 'to_id')
+        return sql_table
+
+
+if __name__ == '__main__':
+    try:
+        connection = psycopg2.connect(
+            user=USER,
+            password=PASSWORD, 
+            host=HOST, 
+            database='workshop')
+
+        cursor = connection.cursor()
+        cursor.execute(DatabaseCreation.create_msg_table())
+        connection.commit()
+    except (psycopg2.OperationalError, psycopg2.errors.DuplicateTable) as error:
+        print('ERROR OCCURED!  \n', error)
+    else:
+        cursor.close()
+        connection.close()
---
+++ models.py
@@ -0,0 +1,7 @@
+
+
+class User:
+    def __init__(self, username, hashed_password)
+    self._id = -1
+    self.username = username
+    self.hashed_password = hashed_password
